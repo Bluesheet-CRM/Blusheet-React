3006c79b6b5b7980eda68f07744c79814b332f4b
Santhosh Kumar <sandymech290023@gmail.com>
2020-11-18 10:18:58 +0530

    remove errors

--- src/utils/base.js
+++ src/utils/base.js
@@ -3,7 +3,6 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/analytics';
 
-;
 var app = firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
 
 export default app;
