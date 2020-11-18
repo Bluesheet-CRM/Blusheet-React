b8b77fcbe15b165e7c049d95d94e7a0ff5ed49ec
Santhosh Kumar <sandymech290023@gmail.com>
2020-11-18 10:18:01 +0530

    env check complete

--- src/utils/base.js
+++ src/utils/base.js
@@ -3,8 +3,7 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/analytics';
 
-
-console.log(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
+;
 var app = firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
 
 export default app;
